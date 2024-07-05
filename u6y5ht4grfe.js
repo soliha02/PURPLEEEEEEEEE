ab56d77e094af9db3dde2cd1d0adc7206041e50b
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-07-05 11:05:00 +0500

    Add files via upload

---
+++ u6y5ht4grfe.js
@@ -0,0 +1,24 @@
+let body = document.querySelector('body')
+let color = prompt('RANG KIRITING!')
+
+body.style.backgroundColor = color
+body.style.textAlign = 'center'
+body.style.marginTop = '250px'
+
+
+let title = document.createElement('h1')
+title.innerHTML = prompt('ISM KIRITING')
+
+body.appendChild(title)
+
+
+
+
+
+
+
+
+
+
+
+
